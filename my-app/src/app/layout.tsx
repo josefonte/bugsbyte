3fbf2af025642ab84a4cd1d59745900875ab601c
José Fonte <73178551+josefonte@users.noreply.github.com>
2024-04-06 00:16:11 +0100

    components

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Knowledge4Knowledge",
+    title: "ServiceFlow",
     description: "",
 };
 
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-    return <div>nsadnajsn</div>;
+    return <div className="flex flex-col m-2 mt-2">nsadnajsn</div>;
 }
