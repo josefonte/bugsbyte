8148b53815e1502e5f7ffaf09f367044d7745242
jotaalvim <joaoafonsoalvim@gmail.com>
2024-04-06 15:37:22 +0100

    novos cards

--- my-app/src/components/mine/proposal-card.tsx
+++ my-app/src/components/mine/proposal-card.tsx
@@ -87,6 +87,7 @@ export default function ProposalCard() {
                     </CardDescription>
                 </div>
             </div>
+            
             <CardFooter />
         </Card>
     );
